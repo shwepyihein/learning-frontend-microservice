bf657577e13e6fb2294ea328f1e3fab803a371b9
shwepyihein <shwepyihein.12@gmail.com>
2021-07-08 23:21:04 +0630

    change post start and testing

--- posts/index.js
+++ posts/index.js
@@ -9,14 +9,15 @@ app.use(bodyParser.json())
 app.get("/posts", (req, res) => {
   res.send(posts)
 })
+// as
 app.post("/posts", (req, res) => {
   const id = randomBytes(4).toString("hex")
   const { title } = req.body
-  post[id] = {
+  posts[id] = {
     id,
     title,
   }
-  res.status(201).send(post[id])
+  res.status(201).send(posts[id])
 })
 
 app.listen(4000, () => {
