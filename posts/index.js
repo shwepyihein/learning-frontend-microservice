49e8ca3467a7d75a7e69657a58a675058b0f9a64
shwepyihein <shwepyihein.12@gmail.com>
2021-07-08 23:12:37 +0630

    post api get and post

---
+++ posts/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { randomBytes } = require("crypto")
+const app = express()
+
+const posts = {}
+app.use(bodyParser.json())
+
+app.get("/posts", (req, res) => {
+  res.send(posts)
+})
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex")
+  const { title } = req.body
+  post[id] = {
+    id,
+    title,
+  }
+  res.status(201).send(post[id])
+})
+
+app.listen(4000, () => {
+  console.log("listening on 4000")
+})
